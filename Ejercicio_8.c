2f643e4dbcbb4b7690d890f89889ad5933c646ff
gonzalo <you@example.com>
2021-07-01 20:08:32 -0300

    Ajedrex hecho!

--- Ejercicio_8.c
+++ Ejercicio_8.c
@@ -5,17 +5,14 @@ int main(int argc, char *argv[]) {
   int n = atoi(argv[1]);
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= n; j++) {
-     if ((i % 2 == 0) || (j % 2== 0))
+    if ((i % 2 == 0 + j % 2== 0)){
         printf("*");
+      }else{
         printf(" ");
-        printf("*");
-        printf(" ");
-       // else
-       //   printf("   ");
     }
 
-
-   printf("\n");
+}
+printf("\n");
   }
 
   return 0;
