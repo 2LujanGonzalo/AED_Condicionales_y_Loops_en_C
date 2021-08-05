9d1d1ffeff3a3032281eb88d7ba1305aa835dbd3
gonzalo <gonzalodlujan@gmail.com>
2021-08-05 09:20:03 -0300

    EJERCICICIOS RESUELTOS

--- Ejercicio_5.c
+++ Ejercicio_5.c
@@ -5,14 +5,13 @@
 
 int main(void){
 
-int i=1001;//Si pongo i=1000 cuando lo ejecute saldrá :1000 1001 1002 1003 1004 1005
-                                                    // 1006 1007 1008 1009 1010 despues sigue bien.
-
+int i=1000;
 
 while (i<2000) {
-printf("%d ",i++);
-if (i%10==0||i%10==5) {
-  printf("%d\n",i++ );
+printf("%d ",i);
+i++;
+if (i%5==0) {
+  printf("\n");
 }
 }
 
